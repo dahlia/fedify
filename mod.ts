2e27ca3194713ed5a52625fe60c990691e51b971
Hong Minhee <hong@minhee.org>
2024-03-10 20:55:32 +0900

    Expose lookupWebFinger()

--- mod.ts
+++ mod.ts
@@ -50,4 +50,4 @@ export * from "./httpsig/mod.ts";
 export * from "./nodeinfo/mod.ts";
 export * from "./runtime/mod.ts";
 export * from "./vocab/mod.ts";
-export { type ResourceDescriptor } from "./webfinger/mod.ts";
+export { lookupWebFinger, type ResourceDescriptor } from "./webfinger/mod.ts";
