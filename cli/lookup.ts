0cfc76fb5016f2221bfd139d1d6182cc39da0036
Hong Minhee <hong@minhee.org>
2024-04-27 16:22:48 +0900

    Add -a/--authorized-fetch option to fedify lookup

--- cli/lookup.ts
+++ cli/lookup.ts
@@ -1,6 +1,17 @@
 import { Command } from "@cliffy/command";
-import { lookupObject } from "@fedify/fedify";
+import {
+  Application,
+  CryptographicKey,
+  type DocumentLoader,
+  generateCryptoKeyPair,
+  getAuthenticatedDocumentLoader,
+  lookupObject,
+  type ResourceDescriptor,
+  respondWithObject,
+} from "@fedify/fedify";
 import { highlight } from "cli-highlight";
+import ora from "ora";
+import { spawnTemporaryServer, type TemporaryServer } from "./tempserver.ts";
 
 export const command = new Command()
   .arguments("<url:string>")
@@ -9,6 +20,7 @@ export const command = new Command()
       "The argument can be either a URL or an actor handle " +
       "(e.g., @username@domain).",
   )
+  .option("-a, --authorized-fetch", "Sign the request with an one-time key.")
   .option("-c, --compact", "Compact the fetched JSON-LD document.", {
     conflicts: ["expand"],
   })
@@ -16,13 +28,77 @@ export const command = new Command()
     conflicts: ["compact"],
   })
   .action(async (options, url: string) => {
-    const object = await lookupObject(url);
-    if (options.compact) {
-      printJson(await object?.toJsonLd());
-    } else if (options.expand) {
-      printJson(await object?.toJsonLd({ expand: true }));
-    } else {
-      console.log(object);
+    const spinner = ora({
+      text: "Looking up the object...",
+      discardStdin: false,
+    }).start();
+    let server: TemporaryServer | undefined = undefined;
+    let loader: DocumentLoader | undefined = undefined;
+    if (options.authorizedFetch) {
+      spinner.text = "Generating a one-time key pair...";
+      const key = await generateCryptoKeyPair();
+      spinner.text = "Spinning up a temporary ActivityPub server...";
+      server = await spawnTemporaryServer((req) => {
+        if (new URL(req.url).pathname == "/.well-known/webfinger") {
+          const jrd: ResourceDescriptor = {
+            subject: `acct:${server!.url.hostname}@${server!.url.hostname}`,
+            aliases: [server!.url.href],
+            links: [
+              {
+                rel: "self",
+                href: server!.url.href,
+                type: "application/activity+json",
+              },
+            ],
+          };
+          return new Response(JSON.stringify(jrd), {
+            headers: { "Content-Type": "application/jrd+json" },
+          });
+        }
+        return respondWithObject(
+          new Application({
+            id: server?.url,
+            preferredUsername: server?.url?.hostname,
+            publicKey: new CryptographicKey({
+              id: new URL("#main-key", server?.url),
+              owner: server?.url,
+              publicKey: key.publicKey,
+            }),
+            manuallyApprovesFollowers: true,
+            inbox: new URL("/inbox", server?.url),
+            outbox: new URL("/outbox", server?.url),
+          }),
+        );
+      });
+      loader = getAuthenticatedDocumentLoader({
+        keyId: new URL("#main-key", server.url),
+        privateKey: key.privateKey,
+      });
+    }
+    try {
+      spinner.text = "Looking up the object...";
+      const object = await lookupObject(url, { documentLoader: loader });
+      spinner.succeed();
+      if (object == null) {
+        console.error("Failed to fetch the object.");
+        if (loader == null) {
+          console.error(
+            "It may be a private object.  Try with -a/--authorized-fetch.",
+          );
+        }
+        Deno.exit(1);
+      }
+      if (options.compact) {
+        printJson(await object.toJsonLd());
+      } else if (options.expand) {
+        printJson(await object.toJsonLd({ expand: true }));
+      } else {
+        console.log(object);
+      }
+    } catch (_) {
+      spinner.fail();
+    } finally {
+      await server?.close();
     }
   });
 
---
+++ cli/tempserver.ts
@@ -0,0 +1,38 @@
+import { openTunnel } from "@hongminhee/localtunnel";
+import { getLogger } from "@logtape/logtape";
+
+const logger = getLogger(["fedify", "cli", "tempserver"]);
+
+export interface TemporaryServer {
+  url: URL;
+  close(): Promise<void>;
+}
+
+export function spawnTemporaryServer(
+  handler: Deno.ServeHandler,
+): Promise<TemporaryServer> {
+  return new Promise((resolve) => {
+    const ac = new AbortController();
+    const server = Deno.serve({
+      handler,
+      port: 0,
+      signal: ac.signal,
+      onListen({ port }) {
+        logger.debug("Temporary server is listening on port {port}.", { port });
+        openTunnel({ port }).then((tun) => {
+          logger.debug(
+            "Temporary server is tunneled to {url}.",
+            { url: tun.url.href },
+          );
+          resolve({
+            url: tun.url,
+            async close() {
+              await server.shutdown();
+              await tun.close();
+            },
+          });
+        });
+      },
+    });
+  });
+}
